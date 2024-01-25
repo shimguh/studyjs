be5062fcce5ae154eae24a12593f950203637603
shimguh <shimguh@naver.com>
2024-01-25 14:01:49 +0900

    studyjs

---
+++ s5.js
@@ -0,0 +1,19 @@
+function Animal(type, name, sound) {
+	this.type = type;
+	this.name = name;
+	this.sound = sound;
+	this.say = function() {
+		console.log(this.sound);
+	};
+	//var gubun = '동물';
+	//let gubun = '동물';
+	const gubun = '동물';
+
+}
+
+const dog = new Animal('개', '멍멍이', '멍멍');
+const cat = new Animal('고양이', '야옹이', '야옹');
+dog.say();
+cat.say();
+
+console.log(dog.gubun);
